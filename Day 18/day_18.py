a5f567aa073757c138aa768d33978a122ba00162
Severag <odonobii@gmail.com>
2023-11-28 21:32:27 -0600

    Update day_18.py
    rewrote day 18 to use a depth-first-search to find the points in water and successfully solved the problem!

--- Day 18/day_18.py
+++ Day 18/day_18.py
@@ -21,8 +21,7 @@ def check(myanswer, answer):
 
 
 def solve(data, is_part1=True): 
-    if not is_part1:
-        return solve2(data)
+    steam_points = get_steam_points(data) if not is_part1 else None
     
     total = 0
     
@@ -30,14 +29,14 @@ def solve(data, is_part1=True):
         adj = list(point)  # adjacent point
         sides = 6
         
-        for idx in range(3):
-            for offset in [-1, 1]:
-                adj[idx] += offset
-                
-                if tuple(adj) in data:
-                    sides -= 1
-                
-                adj[idx] = point[idx]
+        for adj in get_adjacents(point):
+            # side doesn't count if it's bordering another lava point or 
+            # (for part 2) an air pocket (aka a not-steam, not-lave point)
+            # if adj in data or (not is_part1 and adj not in steam_points):
+            if adj in data:
+                sides -= 1
+            elif not is_part1 and adj not in steam_points:
+                sides -= 1
         
         total += sides
     
@@ -45,77 +44,43 @@ def solve(data, is_part1=True):
 
 
 
-def solve2(data):
-    total = 0
-    coords = [set(), set(), set()]
-    empties = set()
+def get_adjacents(point, limits=None):
+    adj = list(point)  # adjacent point
+    adjacents = []
     
-    for point in data:
-        adj = list(point)  # adjacent point
-        sides = 6
-        
-        for idx in range(3):
-            coords[idx].add(point[idx])
+    for idx in range(3):
+        for offset in [-1, 1]:
+            adj[idx] += offset
             
-            for offset in [-1, 1]:
-                adj[idx] += offset
-                
-                if tuple(adj) in data:
-                    sides -= 1
-                else:
-                    empties.add(tuple(adj))
-                
-                adj[idx] = point[idx]
-        
-        total += sides
-    
-    total -= test_air(empties, data)
-    
-    return total
-
-
-
-def test_air(empties, data):
-    is_air = dict(zip(empties, [True]*len(empties)))
-    adjacencies = {val:[] for val in empties}
-    
-    for point in empties:
-        adj = list(point)  # adjacent point
-        
-        for idx in range(3):            
-            for offset in [-1, 1]:
-                adj[idx] += offset
-                this_adj = tuple(adj)
-                
-                if this_adj in data:
-                    pass  # useful to know regardless
-                elif this_adj in empties:
-                    adjacencies[this_adj].append(point)
-                    adjacencies[point].append(this_adj)
-                else:  # exposed to water/steam
-                    is_air[point] = False
-                
-                adj[idx] = point[idx]
-    
-    for point, air_bool in is_air.items():
-        if not air_bool:
-            open_list = [point]
-            closed_list = set()
+            # if we don't care about limits or the new coord is within limits
+            if limits is None or (limits[idx][0] <= adj[idx] <= limits[idx][1]):
+                adjacents.append(tuple(adj))            
             
-            while len(open_list) > 0:
-                pnt = open_list.pop()
-                
-                if pnt not in closed_list:
-                    is_air[pnt] = False  # set pnt's air to be False (borders water space)
-                    
-                    open_list += adjacencies[pnt]  # check all pnt's adjacent empties
-                    
-                    closed_list.add(pnt)
+            adj[idx] = point[idx]
     
-    air_spaces = set(key for key,val in is_air.items() if val)
+    return adjacents
+
+
+
+def get_steam_points(data):    
+    all_points = np.array([list(row) for row in data])
+    min_pnt = np.min(all_points, axis=0) - 1
+    max_pnt = np.max(all_points, axis=0) + 1
+    limits = np.array([min_pnt, max_pnt]).T
     
-    return solve(air_spaces)
-                
+    open_list = [tuple(min_pnt)]
+    closed_list = set()  # list of points in the water/steam cloud
+    
+    while open_list:
+        point = open_list.pop()
+        
+        if point not in closed_list:  # skip if we've been here before
+            # otherwise add all adjacent points that aren't lava to open list
+            open_list += [adj for adj in get_adjacents(point, limits) if adj not in data]
+            
+            closed_list.add(point)
+    
+    return closed_list                
         
             
 
@@ -124,14 +89,12 @@ test_case = read_file('test_case.txt')
 test_case2 = read_file('test_case2.txt')
 puzz_input = read_file('puzzle_input.txt')
 
-# print('Part 1'.center(50,'-'))
-# check( solve(test_case), 64 )
-# check( solve(test_case2), 108 )
-# print(solve(puzz_input))
+print('Part 1'.center(50,'-'))
+check( solve(test_case), 64 )
+check( solve(test_case2), 108 )
+print(solve(puzz_input))
 
 print('\n\n' + 'Part 2'.center(50,'-'))
 check( solve(test_case, False), 58 )
 check( solve(test_case2, False), 90 )
-# print(solve(puzz_input, False))
-
-# 4240 is too high
+print(solve(puzz_input, False))
